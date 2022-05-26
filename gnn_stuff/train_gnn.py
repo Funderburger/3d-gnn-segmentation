1605272b9b771e07cd333062d0fe5319b6dd0f7b
Fun Marian <JasonFunderburger@protonmail.com>
2022-05-26 21:19:25 +0300

    Multi gpu supportgit status !

--- gnn_stuff/train_gnn.py
+++ gnn_stuff/train_gnn.py
@@ -1,3 +1,19 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch_geometric
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import add_self_loops, remove_self_loops
+from torch_geometric.transforms import BaseTransform, Compose, FaceToEdge, KNNGraph, RadiusGraph
+from torch_geometric.data import Data, InMemoryDataset, extract_zip, Dataset, download_url
+from torch_geometric.loader import DataLoader
+import torch.distributed as dist
+
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+
 from logging import root
 from turtle import pos
 import open3d as o3d
@@ -12,25 +28,20 @@ import trimesh
 import numpy as np
 from tqdm import tqdm
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-import torch_geometric
-from torch_geometric.nn import MessagePassing
-from torch_geometric.utils import add_self_loops, remove_self_loops
-from torch_geometric.transforms import BaseTransform, Compose, FaceToEdge, KNNGraph, RadiusGraph
-from torch_geometric.data import Data, InMemoryDataset, extract_zip, DataLoader, Dataset, download_url
-
 import os
 import os.path as osp
 
 from pyntcloud import PyntCloud
 
 import scipy.io
+from zmq import device
 # mat = scipy.io.loadmat('SUNRGBDtoolbox/Metadata/seglistall.mat')
 # mat['seglistall'][0][5]
-torch.cuda.set_device(1)
+# torch.cuda.set_device(1)
+
+
+
+torch.cuda.empty_cache()
 
 class NormalizeUnitSphere(BaseTransform):
     """Center and normalize node-level features to unit length."""
@@ -58,9 +69,8 @@ class NormalizeUnitSphere(BaseTransform):
 
 
 class MyOwnDataset(Dataset):
-    def __init__(self, root,transform=None, pre_transform=None, pre_filter=None):
-        # self.test = test
-        # self.
+    def __init__(self, validation, root, transform=None, pre_transform=None, pre_filter=None):
+        self.validation = validation
         super().__init__(root, transform, pre_transform, pre_filter)
 
     @property
@@ -77,12 +87,13 @@ class MyOwnDataset(Dataset):
         # for filename in self.raw_paths:
         #     files.append(filename)
         self.data = self.raw_paths
+        validation_size = int(len(self.data)*0.3)
 
-        # if self.test:
-        #     return [f'data_test_{i}.pt' for i in self.data]
-        # else:
-        #     return [f'data_{i}.pt' for i in self.data]
-        return [f'data_{i}.pt' for i in range(len(self.data))]
+        # return [f'data_{i}.pt' for i in range(len(self.data))]
+        if self.validation:
+            return [f'val_data_{i}.pt' for i in range(validation_size)]
+        else:
+            return [f'data_{i}.pt' for i in range(len(self.data)-validation_size)]
 
     def download(self):
         pass
@@ -92,14 +103,23 @@ class MyOwnDataset(Dataset):
         # files = []
         # for filename in sorted(os.scandir(self.raw_paths[0]), key=lambda f: f.name):
         #     files.append(filename)
+        
         self.data = self.raw_paths
+        validation_size = int(len(self.data)*0.3)
+        validation_data = self.data[-validation_size:]
+        training_data = self.data[:len(self.data)-validation_size]
         index = 0
-        for pcd_name in tqdm(self.data, total=self.data.__len__()):
+        if self.validation:
+            dataset = validation_data
+        else:
+            dataset = training_data
+
+        for pcd_name in tqdm(dataset, total=dataset.__len__()):
             # pcd_np = np.genfromtxt(pcd_name, delimiter=",")
             pcd_object = PyntCloud.from_file(pcd_name)
             pcd_np = np.array([pcd_object.points.x, pcd_object.points.y, pcd_object.points.z, pcd_object.points.label]).T
 
-             # Get node features
+            # Get node features
             # node_feats = torch.tensor(np.concatenate([pcd_np[:,:3], pcd_np[:,3:6]/255],1)).cuda()
             node_feats = torch.tensor(pcd_np[:,:3]).cuda()
             # Get edge features
@@ -108,12 +128,12 @@ class MyOwnDataset(Dataset):
             torch_points = torch.tensor(pcd_np[:,:3]).cuda()
             edge_index = torch_geometric.nn.knn_graph(
                 torch_points,
-                20,
+                6,
                 None,
                 loop=False,
                 flow='source_to_target',
                 cosine=True,
-                num_workers=1,
+                num_workers=8,
             )
             # Get labels info
             # label = self._get_labels(pcd_np[:,6])
@@ -127,31 +147,35 @@ class MyOwnDataset(Dataset):
                         pos=torch_points
                         )
             
-            # #########################
-            # debug visualization
-            import networkx as nx
-            from torch_geometric.utils.convert import to_networkx
-            datagraph = to_networkx(data)
 
-            node_labels = data.y[list(datagraph.nodes)].cpu().numpy()
+            # ****************************
+            # * debug visualization
+            # ****************************
+            # import networkx as nx
+            # from torch_geometric.utils.convert import to_networkx
+            # datagraph = to_networkx(data)
 
-            import matplotlib.pyplot as plt
-            plt.figure(1,figsize=(14,12)) 
-            nx.draw(datagraph, cmap=plt.get_cmap('Set1'),node_color = node_labels,node_size=4,linewidths=3)
-            # plt.show()
-            plt.savefig(pcd_name[-6:-3]+"40k_graph.png")
-            # ##########################
-            torch.save(data, 
-                        os.path.join(self.processed_dir, 
-                        f'data_{index}.pt'))
-            # if self.test:
-            #     torch.save(data, 
-            #         os.path.join(self.processed_dir, 
-            #                      f'data_test_{index}.pt'))
-            # else:
-            #     torch.save(data, 
-            #         os.path.join(self.processed_dir, 
-            #                      f'data_{index}.pt'))
+            # node_labels = data.y[list(datagraph.nodes)].cpu().numpy()
+
+            # import matplotlib.pyplot as plt
+            # plt.figure(1,figsize=(14,12)) 
+            # nx.draw(datagraph, cmap=plt.get_cmap('Set1'),node_color = node_labels,node_size=4,linewidths=3)
+            # # plt.show()
+            # plt.savefig(pcd_name[-6:-3]+"40k_graph.png")
+            # # ##########################
+            # ***********************************
+            # ***********************************
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+            
+            if self.validation:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'val_data_{index}.pt'))
+            else:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'data_{index}.pt'))
             index +=1
 
     def _get_labels(self, label):
@@ -167,13 +191,14 @@ class MyOwnDataset(Dataset):
         """
 
         data = torch.load(os.path.join(self.processed_dir, 
-                        f'data_{idx}.pt'))  
-        # if self.test:
-        #     data = torch.load(os.path.join(self.processed_dir, 
-        #                          f'data_test_{idx}.pt'))
-        # else:
-        #     data = torch.load(os.path.join(self.processed_dir, 
-        #                          f'data_{idx}.pt'))   
+                        f'data_{idx}.pt')) 
+    
+        if self.validation:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                f'val_data_{idx}.pt'))
+        else:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                f'data_{idx}.pt')) 
         return data
 
 
@@ -412,7 +437,8 @@ class MeshSeg(torch.nn.Module):
         )
 
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+        torch.autograd.set_detect_anomaly(True)
+        x, edge_index = data.x, data.edge_index #.to(torch.cuda.device(self).idx), data.edge_index.to(torch.cuda.device(self).idx)
         x = self.input_encoder(x)
         x = self.gnn(x, edge_index)
         return self.final_projection(x)
@@ -470,7 +496,8 @@ def train(net, train_data, optimizer, loss_fn, device):
         data = data.to(device)
         optimizer.zero_grad()
         out = net(data)
-        loss = loss_fn(out, data.segmentation_labels.squeeze())
+        # loss = loss_fn(out, data.segmentation_labels.squeeze())
+        loss = loss_fn(out, data.y.type(torch.LongTensor).to(device))
         loss.backward()
         cumulative_loss += loss.item()
         optimizer.step()
@@ -520,7 +547,9 @@ def evaluate_performance(dataset, net, device):
     for data in dataset:
         data = data.to(device)
         predictions = net(data)
-        prediction_accuracies.append(accuracy(predictions, data.segmentation_labels))
+        gt_labels = data.y.type(torch.LongTensor).to(device)
+        gt_labels = gt_labels[:,None]
+        prediction_accuracies.append(accuracy(predictions, gt_labels))
     return sum(prediction_accuracies) / len(prediction_accuracies)
 
 @torch.no_grad()
@@ -545,9 +574,11 @@ def load_model(model_params, path_to_checkpoint, device):
             f" not be loaded. {err_msg}"
         )
 
+
 def get_best_model(model_params, dataset, device):
-    path_to_pretrained_model = Path("/content/checkpoint_best_pretrained")
-    path_to_trained_model = Path("/content/checkpoint_best_colab")
+    # Just if you want to compare two models
+    path_to_pretrained_model = Path("gnn_results/checkpoints/best_pretrained_checkpoint")
+    path_to_trained_model = Path("gnn_results/checkpoints/best_checkpoint_model")
 
     pretrained_model = load_model(
         model_params,
@@ -568,7 +599,17 @@ def get_best_model(model_params, dataset, device):
         return pretrained_model
     return trained_model
 
-def main():
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+def main(rank, world_size):
     # ########################################## #
     # all parameters are for metric point clouds #
     # ########################################## #
@@ -593,69 +634,106 @@ def main():
     # o3d.io.write_point_cloud("SUNRGBDtoolbox/pcd_data_testing/sampled_pcd/orig.pcd" , pcd)
 
     # dataset = MyOwnDataset(root='')
+
+    # ***************
+    # ***************
+
+    # try:
+    #     torch.multiprocessing.set_start_method('spawn')
+    # except RuntimeError:
+    #     pass
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    dataset = MyOwnDataset(root='gnn_dataset/pyg_data/')
 
-    print(dataset[0].edge_index.t())
-    print(dataset[0].x)
-    print(dataset[0].y)
-    print(dataset[0].pos)
+    # device = 'cpu'
+    model_params = dict(
+    in_features=3,
+    encoder_features=16,
+    conv_channels=[32, 64, 128, 64],
+    encoder_channels=[16],
+    decoder_channels=[32],
+    num_classes=38, # 37 different classes + 1 class which is for anything else for the SUNRGBD dataset, of course
+    num_heads=38, 
+    apply_batch_norm=True,
+    )
 
-    # ***************
-    # ***************
+    # net = MeshSeg(**model_params).double()
+    # # net = net.double()
+    # # net = net.to(device)
+    # # rank = 0
+    # # world_size = 1
+    # # setup(rank, world_size)
+    # # dist.init_process_group("gloo",rank=0, world_size=1)
+    # # multi_net = torch.nn.DataParallel(net).to(device) #, device_ids=None) #.to(rank), device_ids=[rank])
+    # multi_net = net.to(device)
+    # multi_net = net
+    device= rank
 
-    # model_params = dict(
-    # in_features=3,
-    # encoder_features=16,
-    # conv_channels=[32, 64, 128, 64],
-    # encoder_channels=[16],
-    # decoder_channels=[32],
-    # num_classes=12,
-    # num_heads=12,
-    # apply_batch_norm=True,
-    # )
+    net = MeshSeg(**model_params).double().to(rank)
+    multi_net = DDP(net, device_ids=[rank])
 
-    # net = MeshSeg(**model_params).to(device)
+    root = "gnn_dataset/pyg_data/"
+    pre_transform = Compose([NormalizeUnitSphere()])
+
+    # dataset = MyOwnDataset(validation=True, root='gnn_dataset/pyg_data/')
+
+    # print(dataset[0].edge_index.t())
+    # print(dataset[0].x)
+    # print(dataset[0].y)
+    # print(dataset[0].pos)
+    train_data = MyOwnDataset(
+        root=root,
+        validation= False,
+        pre_transform=pre_transform,
+    )
     
-    # root = "/content"
-    # pre_transform = Compose([FaceToEdge(remove_faces=False), NormalizeUnitSphere()])
+    validation_data = MyOwnDataset(
+        root=root,
+        validation=True,
+        pre_transform=pre_transform,
+    )
 
-    # train_data = MyOwnDataset(
-    #     root=root,
-    #     pre_transform=pre_transform,
-    # )
-    # test_data = MyOwnDataset(
-    #     root=root,
-    #     train=False,
-    #     pre_transform=pre_transform,
-    # )
-    # train_loader = DataLoader(train_data,  shuffle=True)
-    # test_loader = DataLoader(test_data, shuffle=False)
+    train_loader = DataLoader(train_data,  shuffle=True, batch_size=1) #, num_workers=1, batch_size=1)
+    validation_loader = DataLoader(validation_data, shuffle=False, batch_size=1) #, num_workers=1, batch_size=1)
 
-    # lr = 0.001
-    # num_epochs = 50
-    # best_test_acc = 0.0
+    lr = 0.001
+    num_epochs = 100
+    best_test_acc = 0.0
 
-    # optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-    # loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(multi_net.parameters(), lr=lr)
+    loss_fn = torch.nn.CrossEntropyLoss()
 
-    # with tqdm(range(num_epochs), unit="Epoch") as tepochs:
-    #     for epoch in tepochs:
-    #         train_loss = train(net, train_loader, optimizer, loss_fn, device)
-    #         train_acc, test_acc = test(net, train_loader, test_loader, device)
+
+    with tqdm(range(num_epochs), unit="Epoch") as tepochs:
+        for epoch in tepochs:
+            train_loss = train(multi_net, train_loader, optimizer, loss_fn, device)
+            train_acc, test_acc = test(multi_net, train_loader, validation_loader, device)
             
-    #         tepochs.set_postfix(
-    #             train_loss=train_loss,
-    #             train_accuracy=100 * train_acc,
-    #             test_accuracy=100 * test_acc,
-    #         )
-    #         sleep(0.1)
+            tepochs.set_postfix(
+                train_loss=train_loss,
+                train_accuracy=100 * train_acc,
+                test_accuracy=100 * test_acc,
+            )
+            sleep(0.1)
 
-    #         if test_acc > best_test_acc:
-    #             best_test_acc = test_acc
-    #             torch.save(net.state_dict(), "/content/checkpoint_best_colab")
-    # net = get_best_model(model_params, test_loader, device)
+            if test_acc > best_test_acc:
+                best_test_acc = test_acc
+                torch.save(multi_net.state_dict(), "gnn_results/checkpoints/best_checkpoint_model")
+
+    # cleanup()
+    # net = get_best_model(model_params, validation_loader, device)
 
 
 if __name__=='__main__':
-    main()
+    # mp.spawn(main())
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,3"
+
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    world_size = 2
+    mp.spawn(main,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True)
+    # main()
